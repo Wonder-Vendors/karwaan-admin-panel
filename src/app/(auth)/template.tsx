27908372b3394f7f4174fbc96819bd37a5858315
Kunal gaur <kunal.gaur@simplyfi.tech>
2024-01-25 23:01:47 +0530

    Dummy commit

--- src/app/(auth)/template.tsx
+++ src/app/(auth)/template.tsx
@@ -7,10 +7,10 @@ import React from 'react'
 const template = ({children}: {children: React.ReactNode}) => {
   const router = useRouter();
   const {user, token} = locallyStoredVariables();
-  if(user && token) {
-    router.push('/');
-    return;
-  }
+  // if(user && token) {
+  //   router.push('/');
+  //   return;
+  // }
   return (
     <div>{children}</div>
   )
--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -6,18 +6,20 @@ import Navbar from '@/components/shared/Navbar/Navbar'
 import { useRouter } from 'next/navigation'
 import { locallyStoredVariables } from '@/constants/locallyStoredVariables'
 
-const layout = ({children}: {children: React.ReactNode}) => {
+const layout = ({ children }: { children: React.ReactNode }) => {
   const router = useRouter();
-  const {user, token} = locallyStoredVariables();
-if(typeof(window) !== "undefined"){ 
-   if(!user || !token){
-    router.push('/signin');
-    return;
-  }}
+  // const { user, token } = locallyStoredVariables();
+  // if (typeof (window) !== "undefined") {
+  //   if (!user || !token) {
+  //     router.push('/signin');
+  //     return;
+  //   }
+  // }
+
   return (
     <div id={styles.container}>
-        <Navbar/>
-        {children}
+      <Navbar />
+      {children}
     </div>
   )
 }
