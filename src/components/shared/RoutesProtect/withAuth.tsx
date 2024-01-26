ffb3b53fcb1a203a71a8806d5c79cb74299957c1
yash gupta <yashgupta1mole@gmail.com>
2024-01-26 16:53:50 +0530

    resolve some token issue

--- src/components/shared/RoutesProtect/withAuth.tsx
+++ src/components/shared/RoutesProtect/withAuth.tsx
@@ -4,12 +4,12 @@ import {useEffect} from 'react';
 export default function withAuth(Component :any){
     return function withAuth(props:any){
         useEffect(()=>{
-        var token =JSON.parse(localStorage.getItem("token")as string);
+        var token =(localStorage.getItem("token")as string);
         if(!token){
             redirect("/signup");
         }
     },[]);
-    if(typeof(window)!=="undefined"&& !JSON.parse(localStorage.getItem("token")as string)){
+    if(typeof(window)!=="undefined"&& !(localStorage.getItem("token")as string)){
         return null;
     }
     return <Component {...props}/>
