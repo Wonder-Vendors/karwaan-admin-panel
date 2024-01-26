0f2e43359d9d10ce8079c76d916f694821bbe108
yash gupta <yashgupta1mole@gmail.com>
2024-01-27 01:34:04 +0530

    final

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -3,16 +3,8 @@
 import React from 'react'
 import styles from './layout.module.css'
 import Navbar from '@/components/shared/Navbar/Navbar'
-import { useRouter } from 'next/navigation'
-import withAuth from '@/components/shared/RoutesProtect/withAuth'
-// const router=useRouter();
 const layout = ({children}: {children: React.ReactNode}) => {
-//   if(typeof(window) !== "undefined"){
-//    var token = localStorage.getItem('token');
-//    if(!token){
-//     router.push('/signin');
-//    }
-// }
+
   return (
     <div id={styles.container}>
       <Navbar />
@@ -21,4 +13,4 @@ const layout = ({children}: {children: React.ReactNode}) => {
   )
 }
 
-export default withAuth(layout)
+export default layout
